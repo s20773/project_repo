3be484198b733131e44f7c64414836157cf1a71d
s20773 <s20773@pjwstk.edu.pl>
2021-11-03 18:46:46 +0100

    init

---
+++ MainPage/MainPage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainPage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
